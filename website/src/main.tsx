078fe432c65ad265324b662c3b946f9e398e61c1
Emaad Manzoor <emaadmanzoor@gmail.com>
2026-01-06 01:07:47 -0500

    fix import bug

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./styles.css";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
